7a646d14d79aceea2688170d36d30497302dc857
Ryo <93679782+ryo-04@users.noreply.github.com>
2021-12-02 20:41:08 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # 以下のコードを追加してみましょう
+  get 'hello-world' => "foo#hello"
+
 end
